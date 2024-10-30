b94577bcc3b652949b35216feb6fadd62b4b6169
Antonia <93054555+AntoniaPache@users.noreply.github.com>
2024-10-30 09:04:51 -0300

    implementación de ZXinc para generación de QR, falta front

--- src/main/java/com/example/freezer/controller/ProductoController.java
+++ src/main/java/com/example/freezer/controller/ProductoController.java
@@ -1,13 +1,15 @@
 package com.example.freezer.controller;
 
-import com.example.freezer.model.RegistroTemperatura;
-import com.example.freezer.service.ProductoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.freezer.model.RegistroTemperatura;
+import com.example.freezer.service.ProductoService;
 
 @RestController
 public class ProductoController {
@@ -22,5 +24,12 @@ public class ProductoController {
             System.out.println(registroTemperatura.getEmpleado().getId());
         }
         return productoService.getHistorialByProducto(prodID);
+    }
+
+    @GetMapping("/{productoId}/estado")
+    public ResponseEntity<String> getEstadoProducto(@PathVariable Long productoId) {
+        double promedioTemp = productoService.obtenerPromedioTemperatura(productoId);
+        String estado = promedioTemp <= 3.0 ? "Aprobado" : "Desaprobado";
+        return ResponseEntity.ok(estado);
     }
 }
